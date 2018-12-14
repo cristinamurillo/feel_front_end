43d9b3bfc495c1613d89316ae416608dbbd7efc0
Cristina Murillo <murillocam96@gmail.com>
2018-12-14 10:24:16 -0500

    added initial single painting display / styling

--- src/Components/PaintingSelect.js
+++ src/Components/PaintingSelect.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SinglePaint from './SinglePaint'
 import axios from 'axios'
 const PAINTINGS_URL = "http://localhost:3000/paintings/"
 class PaintingSelect extends Component {
@@ -27,14 +28,23 @@ class PaintingSelect extends Component {
             })
     }
 
+    paintingDisplay = () => {
+        axios.get(PAINTINGS_URL)
+            .then(response => {
+                console.log(response.data)
+            })
+    }
+
     render() {
     return (
-        <div>
+        <div class="section">
+        <h3 class="header">Choose an Artwork</h3>
             <form onSubmit={this.submitHandler}>
-                <input type="text" name="img_url" placeholder="Image URL" value={this.state.img_url} onChange ={this.changeHandler}/>
+                <input id="img-url" type="text" name="img_url" placeholder="Image URL" value={this.state.img_url} onChange ={this.changeHandler}/>
                 <input type= "submit" value="Submit"/>
             </form>
-        
+            {/* {this.paintingDisplay()} */}
+            <SinglePaint />
         </div>
     );
     }
---
+++ src/Components/SinglePaint.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+
+
+class SinglePaint extends Component {
+
+   
+    render() {
+    return (
+        <div class="section">
+            <img id= "single-paint" src="https://www.rauantiques.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/_/1_30-8109_1.png.png" alt="Painting"/>
+        </div>
+    );
+    }
+}
+
+export default SinglePaint;
