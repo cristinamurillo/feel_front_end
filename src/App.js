d6fc384320d7ac5f02d0f8adde0fec98a3e611b1
Cristina Murillo <murillocam96@gmail.com>
2018-12-17 15:00:49 -0500

    animation page conditionally renders, mapped colors to App, Animation props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import PaintingSelect from './Components/PaintingSelect'
+import Animation from './Components/Animation'
+import {connect} from 'react-redux'
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <h1>MOOD INDIGO</h1>
-        <PaintingSelect />
+        {this.props.colors.length === 0 ? 
+        <PaintingSelect />:
+        <Animation />
+        }
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  colors: state.animations.colors
+})
+
+
+export default connect(mapStateToProps)(App);
--- src/Components/Animation.js
+++ src/Components/Animation.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {fetchColors} from '../redux/animationActions'
+
 
 class Animation extends Component {
     
-  
+  render(){
+      console.log(this.props)
+      return(
+        <p>animation baby!</p>
+      )
+  }
 
 
 }
+
+const mapStateToProps = state => ({
+    colors: state.animations.colors
+})
+
+export default connect(mapStateToProps)(Animation);
