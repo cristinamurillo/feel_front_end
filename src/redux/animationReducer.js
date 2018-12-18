f225352f785784bfeea63da4443fcb409e64a8f7
Cristina Murillo <murillocam96@gmail.com>
2018-12-18 13:25:35 -0500

    error message for invalid image analysis

--- src/redux/animationReducer.js
+++ src/redux/animationReducer.js
@@ -1,19 +1,23 @@
 import {
     FETCH_COLORS_BEGIN,
-    FETCH_COLORS_SUCCESS
+    FETCH_COLORS_SUCCESS,
+    FETCH_COLORS_FAILURE
 } from './animationActions'
 
 const initialState = {
     colors: [],
-    loading: false
+    loading: false,
+    error: null
 }
 
 const fetchedColors = (state=initialState, action) => {
     switch(action.type){
         case FETCH_COLORS_BEGIN:
-            return {...state, loading: true}
+            return {...state, loading: true, error: null}
         case FETCH_COLORS_SUCCESS: 
             return {...state, loading: false, colors: action.payload.colors}
+        case FETCH_COLORS_FAILURE:
+            return {...state, loading: false, error: true, colors: []}
         default:
             return state
     }
