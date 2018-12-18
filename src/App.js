14e2a8850fadcfc000616f8cc0b9103bd39513aa
Cristina Murillo <murillocam96@gmail.com>
2018-12-17 19:55:31 -0500

    added back button, styling changes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 import PaintingSelect from './Components/PaintingSelect'
 import Animation from './Components/Animation'
 import {connect} from 'react-redux'
+import video from './fog.mp4'
 
 class App extends Component {
   render() {
 
     return (
       <div className="App">
+      {/* <video autoPlay muted loop id="backVid">
+        <source src={video} type="video/mp4"/>
+      </video> */}
         {this.props.colors.length === 0 ? 
         <div>
-        <h1>MOOD INDIGO</h1>
+        <h1 className="fade-in">MOOD INDIGO</h1>
         <PaintingSelect /></div>:
-        <Animation />
+        <div className="fade-in">
+        <Animation/>
+        </div>
         }
       </div>
     );
