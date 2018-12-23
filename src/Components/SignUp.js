8466cbf7303bfe73acd82e6483668f2950f98eab
Cristina Murillo <murillocam96@gmail.com>
2018-12-23 18:25:17 -0500

    began to set up sign up, adjusted login styling

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -4,9 +4,24 @@ import axios from 'axios'
 
 class SignUp extends Component {
 
+    state = {
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+        confirm_password: ""
+    }
+
     render(){
         return(
-            <p>it's signup baby</p>
+            <div className="section">
+                <p>it's signup baby</p>
+                <form onSubmit={this.submitHandler}>
+                <input type="text" name="email" placeholder="E-mail" value={this.state.email} onChange={this.changeHandler}/>
+                <input type="text" name="password" placeholder="Password" value={this.state.password} onChange={this.changeHandler}/>
+                <input type= "submit" value="Submit"/>
+                </form>
+            </div>
         )
     }
 
