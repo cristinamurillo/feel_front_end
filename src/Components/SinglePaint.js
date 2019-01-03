6fc1db6f1865819546548bf3848731dcf4b3d920
Cristina Murillo <murillocam96@gmail.com>
2019-01-03 10:56:56 -0500

    can see artwork info in single image display

--- src/Components/SinglePaint.js
+++ src/Components/SinglePaint.js
@@ -10,7 +10,8 @@ class SinglePaint extends Component {
 
     state = {
         paintsSeen: [0],
-        currentPaint: Math.floor(Math.random()*this.props.paintings.length)
+        currentPaint: Math.floor(Math.random()*this.props.paintings.length),
+        moreInfo: false
     }
 
     componentDidMount() {
@@ -22,6 +23,12 @@ class SinglePaint extends Component {
         this.props.dispatch(fetchColors(img.id))
         this.props.dispatch(postJoin(this.props.user.id, img.id))
         this.props.history.push('/animation')
+    }
+
+    infoClickHandler = () => {
+        this.setState({
+            moreInfo: !this.state.moreInfo
+        })
     }
 
     randomNum = (array) => {
@@ -40,11 +47,28 @@ class SinglePaint extends Component {
    
     render() {
         let img = this.props.paintings[this.state.currentPaint]
+        let attr = img.attributes
+        let hasAttr = () => { if(attr.title || attr.description || attr.artist){
+                                return true
+                                } return false
+                            }
+        console.log(hasAttr())
         let url = img.attributes["img-url"]
     return (
         <div className="section">
             <img onClick={this.clickHandler} className="single-paint" data-id={img.id} src={url} alt="Painting"/>
-            <button onClick={() => this.randomNum(this.props.paintings)} className="med-button" id="anotha">Another One</button>
+            {this.state.moreInfo && 
+            <div>
+                <h3>{attr.title && attr.title}</h3>
+                <h4>{attr.artist && attr.artist}</h4>
+                <p>{attr.description && attr.description}</p>
+                <p>{!hasAttr() && `No information available for this artwork, try right clicking and select 'Search Google for Image'`}</p>
+            </div>
+            }
+            <div className="horiz-flex">
+                <button onClick={() => this.randomNum(this.props.paintings)} className="med-button" id="anotha">Another One</button>
+                <button onClick={this.infoClickHandler} className="med-button" id="anotha">{this.state.moreInfo ? "Hide Info": "Artwork Info"}</button>
+            </div>
         </div>
     );
     }
